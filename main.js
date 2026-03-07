c3a8d47342f26201aa49c0ccb982f3ef89a13726
Nguyen Hung <hung2k4hytq@gmail.com>
2026-03-07 17:32:52 +0700

    init project

---
+++ main.js
@@ -0,0 +1,72 @@
+gsap.registerPlugin(ScrollTrigger);
+const btnMenu = document.querySelector(".btn-menu");
+const menu = document.querySelector(".model-menu");
+const btnClose = document.querySelector(".btn-close");
+const header = document.querySelector("header");
+const tl = gsap.timeline({ paused: true });
+tl.to(".logo", { scale: 0, duration: 0.5, transformOrigin: "left center" }).to(
+  "header",
+  { top: "-155px" },
+  "<",
+);
+const vh = window.innerHeight;
+ScrollTrigger.create({
+  start: "top -80",
+  onUpdate: (self) => {
+    if (self.direction === 1) {
+      tl.play();
+    } else {
+      tl.reverse();
+      if (scrollY > vh) {
+        header.classList.add("active");
+      } else {
+        header.classList.remove("active");
+      }
+    }
+  },
+});
+btnMenu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  //   tl.to(".deco-connect", { scale: 1, duration: 5, ease: "power2.inOut" });
+});
+btnClose.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+const stickyItems = document.querySelectorAll(".sticky-container .item");
+function isInMiddle(el) {
+  const rect = el.getBoundingClientRect();
+  const middle = vh / 2;
+  return rect.top <= middle && rect.bottom >= middle;
+}
+const pinSpacer = document.querySelector(".right-col img");
+function changeImageSticky(url) {
+  pinSpacer.src = url;
+}
+function handleSticky() {
+  stickyItems.forEach((el) => {
+    if (isInMiddle(el)) {
+      const previousElement = el.previousElementSibling;
+      if (previousElement) {
+        previousElement.style.opacity = 0.4;
+      }
+      el.style.opacity = 1;
+
+      changeImageSticky(el.dataset.src);
+    }
+  });
+}
+window.addEventListener("scroll", () => {
+  handleSticky();
+});
+
+gsap.to(".banner-img", {
+  y: 600, // banner kéo lên 100px
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".banner-img",
+    start: "top top",
+    end: "+=1200",
+    scrub: true,
+  },
+});
