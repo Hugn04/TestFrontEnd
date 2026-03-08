5254448e6befb2b8e432b7a3713bf2fa02b8b009
Nguyen Hung <hung2k4hytq@gmail.com>
2026-03-08 23:47:13 +0700

    section 7 & section-8

--- main.js
+++ main.js
@@ -88,9 +88,11 @@ function getRotate(el) {
 // Section 5
 const buttons = document.querySelectorAll(".pagination-btn");
 const swiperContainer = document.querySelector(".swiper-container");
+const image = document.querySelector(".image-container .image");
 buttons.forEach((btn, index) => {
   const imgElement = btn.querySelector("img");
   btn.addEventListener("click", function () {
+    image.src = btn.dataset.image;
     const buttonActive = document.querySelector(".pagination-btn.active");
     buttonActive.classList.remove("active");
     buttonActive.querySelector("img").src =
@@ -107,5 +109,80 @@ buttons.forEach((btn, index) => {
 
     // thêm active mới
     this.classList.add("pagination-btn-active");
+  });
+});
+
+const dataPeople = [
+  {
+    descripion: `WATG rất vui khi được làm việc với đội ngũ của Ami&M trong dự án độc đáo và đặc biệt này.
+                Chúng tôi hy vọng sẽ được tiếp tục đồng hành cùng Ami&M trên hành trình hiện thực hóa tầm
+                nhìn cũng như hỗ trợ Ami&M ở những bước triển khai dự án tiếp theo. Nó hoàn toàn khác biệt
+                so với hầu hết các dự án của WATG đã từng thực hiện, điều này khiến công việc trở nên thú vị hơn
+                và chúng tôi đã được truyền cảm hứng rất nhiều từ các bạn.`,
+    name: "Ông Robert Day",
+    role: "Nguyên Giám đốc điều hành khu vực Châu Á - Thái Bình Dương, WATG",
+    image:
+      "https://amigroup.com.vn/wp-content/uploads/2024/09/Doi-tac_WATG_avt.png",
+  },
+  {
+    descripion: `Trải qua một năm đồng hành cùng Ami&M trong dự án chuyển đổi số, chúng tôi thực sự ấn tượng
+                với phong cách làm việc chuyên nghiệp và những câu chuyện đầy cảm hứng. Không chỉ tập trung tìm
+                kiếm giải pháp, Ami&M luôn thật tâm thấu hiểu và sẵn sàng chia sẻ với những thách thức của
+                Đối tác, khơi gợi ý tưởng giúp Base phá vỡ giới hạn nội tại, tạo động lực cho những sáng tạo và
+                câu hỏi làm thế nào để tốt hơn. `,
+    name: "Bà Nguyễn Loan Anh",
+    role: "Giám đốc Tư vấn chuyển đổi số Base.vn",
+    image:
+      "https://amigroup.com.vn/wp-content/uploads/2024/09/Doi-tac_Base_avt-1.png",
+  },
+  {
+    descripion: `Với vai trò là Đối tác tư vấn và thiết kế cấu phần giáo dục tại The Ami Village, FAROS hân hạnh
+                được đồng hành với Ami&M từ những ngày rất sớm. Chúng tôi cảm nhận được niềm say sưa của
+                Ami&M dành cho giáo dục, trân quý sự đầu tư và làm việc nghiêm túc của các bạn để kiến tạo
+                nên một không gian kết nối, trân trọng thiên nhiên như trân trọng gia đình và những mối dây
+                thương yêu mà chúng ta được ban tặng trong đời. `,
+    name: "Bà Nguyễn Thúy Uyên Phương",
+    role: "Founder FAROS Education & Consulting",
+    image:
+      "https://amigroup.com.vn/wp-content/uploads/2024/09/Doi-tac_Faros_avt.png",
+  },
+  {
+    descripion: `Hành trình hợp tác của Ami&M và RMA hướng tới việc phát triển và làm mới thương
+     hiệu Ami&M với chiến lược hình ảnh mới mẻ và bám sát tinh thần của Tập đoàn ở 
+     thời điểm hiện tại. Trong quá trình làm việc, tôi thấy được tinh thần kiên 
+     định và chuyên nghiệp của đội ngũ Ami&M. Tôi phải nói rằng sự hợp tác công việc
+      của chúng ta thật “khó khăn” và tuyệt vời. “Khó khăn” bởi vì không hề có giới 
+      hạn về lượng thông tin mà Ami&M cung cấp cho chúng tôi, cũng như không giới 
+      hạn lượng thời gian chia sẻ mà các bạn dành cho chúng tôi. Tuyệt vời bởi từ 
+      những chia sẻ của các bạn, chúng tôi học được cách để hình thành và chia sẻ 
+      những ý tưởng. Chúng tôi nhận thấy Ami&M có thái độ làm việc rất cởi mở và 
+      sẵn sàng kết hợp các ý tưởng của chúng tôi với ý tưởng nội bộ của các bạn.
+       Trong suốt các cuộc thảo luận sôi nổi giữa chúng ta, tôi cảm nhận rõ ràng
+        năng lượng và tinh thần đáng ngưỡng mộ của việc “Làm thế nào để tốt hơn?”
+           – một tinh thần luôn hướng tới điều tốt nhất trong từng bước đi.`,
+    name: "Ông Richard Moore",
+    role: "Chủ tịch Richard Moore Associates",
+    image:
+      "https://amigroup.com.vn/wp-content/uploads/2024/09/Doi-tac_RMA_avt.png",
+  },
+];
+
+const listThumContainer = document.querySelectorAll(
+  ".section-8 .content-container .thumb-container .item-container",
+);
+const famousContainer = document.querySelector(".famous-word-container");
+
+listThumContainer.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    document
+      .querySelector(".thumb-container .item-container.active")
+      .classList.remove("active");
+    famousContainer.querySelector("h3").textContent =
+      dataPeople[index].descripion;
+    famousContainer.querySelector("strong").textContent =
+      dataPeople[index].name;
+    famousContainer.querySelector("span").textContent = dataPeople[index].role;
+    document.querySelector(".item-preview img").src = dataPeople[index].image;
+    btn.classList.add("active");
   });
 });
